5263a668917f5db50ca4f35ffba207b71b71e6db
12345678+RandomInternetUser3000@users.noreply.github.com <official.coolman.yt@gmail.com>
2025-11-02 17:59:43 +1100

    subscribe button update

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.getElementById("btn").addEventListener("click", () => {
-  alert("It works!");
+  alert("the button is buttoning, how cool!");
 });
+
+const subscribeBtn = document.getElementById("btn2");
+if (subscribeBtn)
+  subscribeBtn.addEventListener("click", () => {
+    window.location.href = "https://www.youtube.com/@COOLmanGamer";
+  });
