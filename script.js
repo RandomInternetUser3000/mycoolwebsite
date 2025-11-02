b26b3bafe8f90d6601f4bca8baa87bad2b23823f
COOLmanYT <sinaaghabagheri@hotmail.com>
2025-11-03 10:40:39 +1100

    testing the button using html instead of

--- script.js
+++ script.js
@@ -2,8 +2,3 @@ document.getElementById("btn").addEventListener("click", () => {
   alert("the button is buttoning, how cool!");
 });
 
-const subscribeBtn = document.getElementById("btn2");
-if (subscribeBtn)
-  subscribeBtn.addEventListener("click", () => {
-    window.location.href = "https://www.youtube.com/@COOLmanGamer?sub_confirmation=1";
-  });
