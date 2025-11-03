db04ec6f11419b9786b280171baf5c4029175ccc
COOLmanYT <sinaaghabagheri@hotmail.com>
2025-11-03 12:14:38 +1100

    version 0.1.1

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-const youtubeBtn = document.getElementById("youtube-btn");
-if (youtubeBtn) {
-  youtubeBtn.addEventListener("click", () => {
-    window.location.href = "https://www.youtube.com/@COOLmanGamer?sub_confirmation=1";
-  });
-}
+
 
